506fd70d86553071e12ed85cc8df07f0b3897998
zod202 <130629708+zod202@users.noreply.github.com>
2023-05-25 19:50:58 +0800

    Update model-config.tsx

--- app/components/model-config.tsx
+++ app/components/model-config.tsx
@@ -10,7 +10,7 @@ export function ModelConfigList(props: {
 }) {
   return (
     <>
-      {/* <ListItem title={Locale.Settings.Model}>
+      {<ListItem title={Locale.Settings.Model}>
         <Select
           value={props.modelConfig.model}
           onChange={(e) => {
@@ -28,7 +28,7 @@ export function ModelConfigList(props: {
             </option>
           ))}
         </Select>
-      </ListItem> */}
+      </ListItem> }
       {/* <ListItem
         title={Locale.Settings.Temperature.Title}
         subTitle={Locale.Settings.Temperature.SubTitle}
